8e7c84bb33d2e1820470f88a127dd6d3516bf97b
Shagufta Fathima Mohammed <140694802+ShaguftaFathima@users.noreply.github.com>
2025-01-04 10:28:56 +0530

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Stock"; 
+$port = 3306;
+
+
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the Stock database.";
+}
+?>
